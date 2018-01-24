ee6f7e7cffc228f787faca50985719b10a2cf0fb
John <John@DESKTOP-EN6IBAH>
2018-01-24 23:42:39 +0100

    Add project files.

---
+++ LoggerLite.EventLog/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerLite.EventLog
+{
+    public class Class1
+    {
+    }
+}
